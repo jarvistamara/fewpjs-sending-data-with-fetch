a7bae96d911ed1f6ca096b0c9ce832e91cdfe548
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-03-29 12:27:00 -0400

    commit tree 6d32915f2a8d153137d6a8892eefc2e131172b4d

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) { // CREATE A FUNCTION THAT ACCEPT TO ARGUEMENTS - NAME, EMAIL
+    return fetch('http://localhost:3000/users', { // FETCH USERS HTML T0 SEND INFO
+        method: 'POST', // POST TO SEND INFO TO SERVER
+        headers: { // HEADERS ALWAYS NEEDED
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ // MAKE POST A STRING
+            name, 
+            email
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object['id']
+    })
+    .catch(function(error) {
+        alert('Bad things!')
+        document.body.innerHTML = error.message
+    });
+};
+
+submitData();
